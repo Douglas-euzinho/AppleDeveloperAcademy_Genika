0f041d85aad6cd030187ad68bedba252f4dc6c9b
Douglas Figueirôa <douguerpw2@hotmail.com>
2022-06-21 19:00:41 -0300

    M0-190 feat: first try to make DataCollector

---
+++ Mini06-watchOSApp WatchKit App/Model/ProtocolDataCollector.swift
@@ -0,0 +1,13 @@
+//
+//  ProtocolDataCollector.swift
+//  Mini06-watchOSApp WatchKit Extension
+//
+//  Created by Douglas Figueirôa on 21/06/22.
+//
+
+import Foundation
+
+
+protocol ProtocolDataCollector {
+    func sendToCoreData()
+}
